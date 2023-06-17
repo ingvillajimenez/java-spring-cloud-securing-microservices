298d7adc6602494b95b978a778206fe2d93709bb
Juan Villa <ing.villa.jimenez@gmail.com>
2023-06-17 15:46:23 -0600

    projects done

---
+++ SecureService3-1/src/main/java/com/example/demo/SecureService31Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureService31Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureService31Application.class, args);
+	}
+
+}
---
+++ SecureService3/src/main/java/com/example/demo/SecureService3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureService3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureService3Application.class, args);
+	}
+
+}
